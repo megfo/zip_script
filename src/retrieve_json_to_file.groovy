d96c9bd69baa8a6bc675407f3beff4c5bf629d10
Meghan Foley <mfoley@Admins-MacBook-Pro-7.local>
2016-07-13 21:59:57 -0400

    cycle through to get all jsons

--- src/retrieve_json_to_file.groovy
+++ src/retrieve_json_to_file.groovy
@@ -5,29 +5,49 @@ import groovy.json.JsonBuilder
  * Created by mfoley on 7/10/16.
  */
 
-String base = "http://lxqgk01.nanigans.com:84/gk/sites/4/generic_attributes/"
-String attribute = "locations.zips"
-String fileBase = "/Users/mfoley/"
+String base = "http://lxqgk01.nanigans.com/gk/sites/76218/generic_attributes/"
+String fileBase = "/Users/mfoley/ng_refactor/before/generic_attributes"
 File f
 String apiString
 URL apiUrl
 
-apiString = base + attribute
+attributeID = new String()
+
+// build the url
+apiString = base
 apiUrl = new URL(apiString)
 
-f = new File(fileBase + attribute + '.json')
+// create the file that will be output to
+f = new File(fileBase + '.json')
 f.createNewFile()
 writer = new PrintWriter(f)
 
-// read json from url
+// read the json from the url
 def content = new JsonSlurper().parse(apiUrl)
 
-// write  json to file
+// write the json to the file
 f.write(new JsonBuilder(content).toPrettyString())
 
-// loop through the entire original json file
-originalJson.each{ originalJsonAttribute ->
+writer.close()
 
-    // keep track of all the attributes we have checked already
-    attributeID = originalJsonAttribute['id']
-    attributeIDList << attributeID
+// loop through the generic_attributes json
+content.each{ jsonAttribute ->
+    attributeID = jsonAttribute['id']
+
+    // build the url
+    apiString = base + attributeID
+    apiUrl = new URL(apiString)
+
+    // create the file that will be output to
+    f = new File(fileBase + '_' + attributeID + '.json')
+    f.createNewFile()
+    writer = new PrintWriter(f)
+
+    // read json from the url
+    content = new JsonSlurper().parse(apiUrl)
+
+    // write the json to the file
+    f.write(new JsonBuilder(content).toPrettyString())
+
+    writer.close()
+}
